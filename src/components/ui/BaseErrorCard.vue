5ad7c676fd7ada2571213375d6c5210f66a24dbf
theexiile1305 <development@michifuchs.com>
2020-11-27 16:29:06 +0100

    feat: added error pages

---
+++ src/components/ui/BaseErrorCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <pv-card class="p-shadow-10">
+    <template #header>
+      <div class="img-container">
+        <slot name="header"></slot>
+      </div>
+    </template>
+    <template #title>
+      <slot class="p-text-bold" name="title"></slot>
+    </template>
+    <template #content>
+      <slot class="p-text-light" name="content"></slot>
+    </template>
+    <template #footer>
+      <pv-button
+        class="p-shadow-10"
+        label="Get me out of there!"
+        @click="redirect('/')"
+      ></pv-button>
+    </template>
+  </pv-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirect(path) {
+      this.$router.push(path);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.img-container {
+  padding: 2rem;
+}
+</style>
---
+++ src/pages/error/ServerError.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-error-card>
+    <template #header>
+      <img src="@/assets/serverError.png" alt="graphic-server-error" />
+    </template>
+    <template #title>
+      Internal Server Error!
+    </template>
+    <template #content>
+      Our server is on a break
+    </template>
+  </base-error-card>
+</template>
+
+<script>
+import BaseErrorCard from '../../components/ui/BaseErrorCard.vue';
+
+export default {
+  components: {
+    BaseErrorCard
+  }
+};
+</script>
